8c9009e1c195518523041fc04dca97b3315f567b
adiel <adielbot@outlook.com>
2017-01-04 14:38:10 +0200

    check

--- app/src/main/java/adiel/rectrain/MainActivity.java
+++ app/src/main/java/adiel/rectrain/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lv = (ListView) findViewById(R.id.lv);
+        ListView lv = (ListView) findViewById(R.id.lv);//llll
 
         final String[] values = new String[] {
                 "CustomAlertDialog",
